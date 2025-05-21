68114146c2ca43696dbf623a9f998bb52f69f58c
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-21 20:28:33 +0900

    チェックボックスなどのコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
 import { Button } from "@mui/material";
+import TaskItem from "./TaskItem.tsx";
 
 function App() {
-  return <Button variant="contained">Contained</Button>;
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+      <div>
+        <TaskItem label="My Task" />
+      </div>
+    </>
+  );
 }
 
 export default App;
