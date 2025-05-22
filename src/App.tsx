aa0d3d2c9ada2a6a7a9d220a427033fdc6441387
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-22 17:35:00 +0900

    label をTodoに変更

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,8 @@ import TaskItem from "./TaskItem.tsx";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div>
         <TaskItem label="ToDo" />
-=======
-      <Button variant="contained">Contained</Button>
-      <div>
-        <TaskItem label="My Task" />
->>>>>>> 68114146c2ca43696dbf623a9f998bb52f69f58c
       </div>
     </>
   );
