7cc31454efbd90e6406c39e9e593a12e9bb08fe3
AOi126 <yamaa439966@gmail.com>
2025-05-23 14:06:22 +0900

    新規追加ボタンを作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Header from "./Header";
+import CircleAddButton from "./AddButton";
 
 function App() {
   return (
-    <>
+    <Box position={"relative"} height={"90vh"}>
       <Header />
       <Button variant="contained">Contained</Button>
-    </>
+      <CircleAddButton />
+    </Box>
   );
 }
 
