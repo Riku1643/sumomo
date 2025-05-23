1e5f852df72842369e42ba5b8e5e71947ce18b79
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-23 11:06:16 +0900

    編集、キャンセルボタンの機能

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -3,15 +3,40 @@ import Checkbox from "@mui/material/Checkbox";
 import { IconButton, Typography } from "@mui/material";
 import mIcon from "./assets/img_1747816091.png";
 import eIcon from "./assets/image 1.png";
+import { useState } from "react";
+import { InputBase } from "@mui/material";
 
 interface TaskItemProps {
   label: string;
 }
 
 const TaskItem = ({ label }: TaskItemProps) => {
+  const [Editing, sEditing] = useState(false);
+  const [Text, sText] = useState(label);
+  const [Backuptext, sBackupText] = useState(label);
+  const sEditClick = () => {
+    if (Editing) {
+      sEditing(false);
+    } else {
+      sBackupText(Text);
+      sEditing(true);
+    }
+  };
+  const sCancelClick = () => {
+    sText(Backuptext);
+    sEditing(false);
+  };
+  const Change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    sText(e.target.value);
+  };
+  const Keydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      sEditing(false);
+    }
+  };
   return (
     <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
-      <Box position="relative" top={2}>
+      <Box position="relative" marginTop={2}>
         <Checkbox
           slotProps={{
             input: { "aria-label": "Checkbox demo" },
@@ -25,12 +50,30 @@ const TaskItem = ({ label }: TaskItemProps) => {
           }}
         />
       </Box>
-      <Box sx={{ width: 275 }}>
-        <Typography>{label}</Typography>
+      <Box sx={{ width: 275 }} marginTop={2}>
+        {Editing ? (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputBase
+              value={Text}
+              onChange={Change}
+              onKeyDown={Keydown}
+              autoFocus
+              placeholder="タスクを入力"
+            />
+            <button onClick={sCancelClick}>cancel</button>
+          </Box>
+        ) : (
+          <Typography>{Text}</Typography>
+        )}
       </Box>
-      <Box>
-        <IconButton aria-label="edit">
-          <img src={eIcon} alt="edit icon" style={{ objectFit: "contain" }} />
+      <Box marginTop={2}>
+        <IconButton aria-label="edit" onClick={sEditClick}>
+          <img src={eIcon} alt={"edit icon"} style={{ objectFit: "contain" }} />
         </IconButton>
         <IconButton aria-label="message">
           <img
