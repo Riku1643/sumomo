f470f7b3807f70b3d82031c5c42398356402dbf8
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-24 15:21:37 +0900

    追加モーダル

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { Box, Button } from "@mui/material";
 import TaskItem from "./TaskItem.tsx";
 import Header from "./Header";
 import CircleAddButton from "./AddButton";
+import { useState } from "react";
+import SModal from "./plus-modal.tsx";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box position={"relative"} height={"90vh"}>
       <Header />
-      <CircleAddButton />
+      <CircleAddButton onClick={handleOpen} />
       <TaskItem label="Todo" />
+      <SModal open={open} onClose={handleClose} />
     </Box>
   );
 }
